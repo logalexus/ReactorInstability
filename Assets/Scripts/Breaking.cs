55fe2adf0dae9c4fd04d7d9f6aeb6082032259df
logalexus <aliosha.log@yandex.ru>
2021-10-03 00:08:59 +0500

    229

--- Assets/Scripts/Breaking.cs
+++ Assets/Scripts/Breaking.cs
@@ -1,24 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class Breaking : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer _sprite;
+    [SerializeField] private Image _sprite;
+    [SerializeField] private Camera _camera;
 
+    
     public void StartBreaking()
     {
 
     }
     
-    public void FixBreaking()
+    public void FixBreaking(TweenCallback callback)
     {
-        StartCoroutine(ProccesFixing());
+        Vector3 screenPos = _camera.WorldToScreenPoint(Player.Instance.transform.position + Vector3.up * 2);
+        _sprite.transform.position = screenPos;
+        StartCoroutine(ProccesFixing(callback));
     }
 
-    IEnumerator ProccesFixing()
+    IEnumerator ProccesFixing(TweenCallback callback)
     {
-        yield return new WaitForSeconds(10f);
-        
+        _sprite.DOFillAmount(1, 4).OnComplete(callback);
+        yield return new WaitForSeconds(4f);
+        Player.Instance.GetComponent<PlayerMove>().enabled = true;
+        Player.Instance.GetComponent<Iteracter>().enabled = true;
+        _sprite.fillAmount = 0;
     }
 }
---
+++ Assets/Scripts/UI/Screens/BreakingPanel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakingPanel : IteractPanel
+{
+    [SerializeField] private Breaking _breaking;
+    public override void Open()
+    {
+        base.Open();
+        _breaking.FixBreaking(()=>
+        {
+            Close();
+        });
+    }
+
+}
