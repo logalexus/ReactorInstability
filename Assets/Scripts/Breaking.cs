4fec6f4c938f67f25abe92378fd369955e5f3450
logalex <logalex2001@yandex.ru>
2021-10-03 22:40:09 +0500

    23

--- Assets/Scripts/Breaking.cs
+++ Assets/Scripts/Breaking.cs
@@ -3,14 +3,12 @@ using System.Collections;
 using DG.Tweening;
 using UnityEngine.UI;
 using UnityEngine.Events;
-using UnityEngine.Experimental.Rendering.Universal;
 
 public class Breaking : MonoBehaviour
 {
     [SerializeField] private Image _fixingProgressBar, _progressBarBorder;
     [SerializeField] private SpriteRenderer _warningSign;
     [SerializeField] private Camera _camera;
-    [SerializeField] private Light2D _lightReactor, _lightGenerator;
 
     private Sequence _warningSignSeq;
     private float _scaleDelta = 0.2f;
@@ -26,16 +24,8 @@ public class Breaking : MonoBehaviour
             _warningSignSeq = DOTween.Sequence();
             _warningSignSeq.Join(_warningSign.DOFade(1, 0.4f));
             _warningSignSeq.Join(_warningSign.transform.DOScale(new Vector2(transform.localScale.x + _scaleDelta, transform.localScale.y + _scaleDelta), 0.4f)).SetLoops(-1, LoopType.Yoyo);
-<<<<<<< HEAD
             _isCall = true;
             Reactor.Instance.Delta += 10;
-=======
-
-            _lightReactor.intensity = 1;
-            _lightGenerator.intensity = 1;
-
-            isCall = true;
->>>>>>> remotes/origin/master
         }
     }
 
@@ -59,18 +49,10 @@ public class Breaking : MonoBehaviour
             _warningSignSeq.Restart();
         });
         _warningSignSeq.Kill();
-<<<<<<< HEAD
         callbackAfterFix?.Invoke();
         callbackAfterFix = null;
         _isCall = false;
         Reactor.Instance.Delta -= 10;
 
-=======
-        
-        _lightReactor.intensity = 0;
-        _lightGenerator.intensity = 0;
-
-        isCall = false;
->>>>>>> remotes/origin/master
     }
 }
